f876efe334893b84b94c8263317ff29a1a6f8a3e
ramarsala <46982082+ramarsala@users.noreply.github.com>
2019-02-27 13:32:11 -0700

    Changed Head_dim to 1000 and Mouth_height to 20

--- src/Cat.java
+++ src/Cat.java
@@ -5,7 +5,7 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 500;
+	private static final int HEAD_DIMENSION = 1000;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
@@ -14,7 +14,7 @@ public class Cat {
 	private static final int EYE_HEIGHT = 50;
 	private static final int EYE_WIDTH = 25;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
+	private static final int MOUTH_HEIGHT = 20;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
